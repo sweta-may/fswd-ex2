3f452f08d4abb5b8427c7383b31f73d4f0ed5114
sweta <swetasakthivel5@gmail.com>
2026-07-28 11:04:53 +0530

    home page designed and added icons, search bar, for you and following bar with tab click behavior

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-    
+const tabs = document.querySelectorAll('.tab-item');
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('active'));
+        tab.classList.add('active');
+      });
+    });
